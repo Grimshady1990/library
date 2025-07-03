ccec0044a68d4bdf5c2dd1e51286cfc9e9d42bad
Grimshady1990 <frankiedthomas@gmail.com>
2025-07-03 21:55:22 +0100

    Make a start on adding book to library

---
+++ index.js
@@ -0,0 +1,20 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+let raisingSteam = new Book("Raising Steam", "Terry Prachet", 389, true);
+
+
+function addBookToLibrary() {
+  prompt() = new Book();
+
+}
+
+addBookToLibrary();
+
+console.log(myLibrary);
